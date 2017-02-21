93d5bc06daa6357334b913de284fc6980260da5d
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-21 11:41:13 +0100

    Set up Firebase admin

--- server.js
+++ server.js
@@ -3,30 +3,30 @@ const express = require('express'),
 	path = require('path'),
 	bodyParser = require('body-parser'),
 	session = require('express-session');
-	// firebase = require('firebase-admin'),
-	// FirebaseStore = require('connect-session-firebase')(session),
-	// serviceAccount = require('config/serviceAccountKey.json');
+	firebase = require('firebase-admin'),
+	FirebaseStore = require('connect-session-firebase')(session),
+	serviceAccount = require('./config/serviceAccountKey.json');
 require('dotenv').load();
 
 /* --------------- FIREBASE --------------- */
-// firebase.initializeApp({
-// 	// credential: firebase.credential.cert(serviceAccount),
-//     apiKey: process.env.firebaseKey,
-//     authDomain: process.env.firebaseAuth,
-//     databaseURL: process.env.firebaseDB,
-//     storageBucket: process.env.firebaseStore,
-//     messagingSenderId: process.env.firebaseMSG
-// });
-// const db = firebase.database();
+firebase.initializeApp({
+	credential: firebase.credential.cert(serviceAccount),
+    // apiKey: process.env.firebaseKey,
+    // authDomain: process.env.firebaseAuth,
+    databaseURL: process.env.firebaseDB,
+    // storageBucket: process.env.firebaseStore,
+    // messagingSenderId: process.env.firebaseMSG
+});
+const db = firebase.database();
 
 /* --------------- EXPRESS --------------- */
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
-	// store: new FirebaseStore({
-	// 	database: db
-	// }),
+	store: new FirebaseStore({
+		database: db
+	}),
 	secret: process.env.secret,
 	resave: true,
 	saveUninitialized: true
